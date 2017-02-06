d198cd261ed866a686fd9a687d42cf61caba7fbf
lorenzo farnararo <baldarn@lorenzos-iMac.local>
2017-02-06 15:11:34 +0100

    fix empty string save as value

--- lib/cassandra_object/adapters/cassandra_adapter.rb
+++ lib/cassandra_object/adapters/cassandra_adapter.rb
@@ -136,14 +136,14 @@ module CassandraObject
         queries = []
         # puts attributes
         attributes.each do |column, value|
-          if value.present?
+          if !value.nil?
             query = "INSERT INTO #{table} (#{primary_key_column},column1,value) VALUES (?,?,?)"
-            query += " USING TTL #{ttl.to_s}" if ttl.present?
+            query += " USING TTL #{ttl.to_s}" if !ttl.nil?
             args = [id.to_s, column.to_s, value.to_s]
 
             queries << {query: query, arguments: args}
           else
-            queries << {query: "DELETE FROM #{table} WHERE #{primary_key_column} = ? AND column1= ?", arguments: [id.to_s, column.to_s]} if value.nil?
+            queries << {query: "DELETE FROM #{table} WHERE #{primary_key_column} = ? AND column1= ?", arguments: [id.to_s, column.to_s]}
           end
         end
         execute_batchable(queries)
